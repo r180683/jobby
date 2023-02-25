09ea1b61af3baf497046ae594a0ce395396dbeb0
r180683 <r180683@rguktrkv.ac.in>
2023-02-25 10:34:41 +0000

    commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,32 @@
+import {withRouter, Link} from 'react-router-dom'
+import Header from '../Header'
+
+import './index.css'
+
+const Home = props => {
+  const onFindJobs = () => {
+    const {history} = props
+    history.replace('/jobs')
+  }
+
+  return (
+    <div className="home-container">
+      <Header />
+      <div className="home-text-container">
+        <div className="home-text-size">
+          <h1 className="home-head">Find The Job That Fits Your Life</h1>
+          <p className="home-description">
+            Millions of people are searching for jobs,salary information,company
+            reviews.Find the job that fits your abilities and potential.
+          </p>
+          <Link to="/jobs">
+            <button className="find-jobs-btn" type="button">
+              Find Jobs
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default withRouter(Home)
